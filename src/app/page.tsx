4065449672dcbf091ae90bbba361e9601d0946fe
omer4ruq <omerfaruq4@gmail.com>
2026-06-11 16:58:23 +0100

    layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { Navbar } from "@/components/shared/Navbar";
 import { Hero } from "@/components/pages/Home/Hero";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col bg-zinc-50 dark:bg-black">
-      <Navbar />
-      <main className="flex-1 flex flex-col">
-        <Hero />
-      </main>
-    </div>
-  );
+  return <Hero />;
 }
-
-
