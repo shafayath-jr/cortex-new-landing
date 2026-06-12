9da318ea46b34ee49df7b5470a3f230e21dfc0f7
Shafayath Jamil Rafi <shafayath.r@penglobalbd.com>
2026-06-12 15:50:39 +0100

    feat: add Our Products section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/pages/Home/Hero";
 import { HowItWorks } from "@/components/pages/Home/HowItWorks";
 import { NameChecker } from "@/components/pages/Home/NameChecker";
+import { OurProducts } from "@/components/pages/Home/OurProducts";
 import { Footer } from "@/components/shared/Footer";
 import { Navbar } from "@/components/shared/Navbar";
 
@@ -11,9 +12,8 @@ export default function Home() {
       <main className="flex-1 flex flex-col">
         <Hero />
         <NameChecker />
+        <OurProducts />
         <HowItWorks />
-        <div className="h-screen border-t border-[#F4EEE4]" />
-        {/* Spacer before footer */}
       </main>
       <Footer />
     </div>
